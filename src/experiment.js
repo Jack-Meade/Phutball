22317102b3bb560b402311e177e106fae4f880f2
Jack Meade <jack-meade@outlook.com>
2020-11-04 19:57:57 +0000

    Refactor functions which create elements to separate file

--- src/experiment.js
+++ src/experiment.js
@@ -1,3 +1,5 @@
+import * as ce from './lib/create_element.js'
+
 (function() {
 
   document.addEventListener('DOMContentLoaded', init, false)
@@ -28,7 +30,8 @@
         console.log(`Error ${xhr.status}: ${xhr.statusText}`)
       } else {
         var response = JSON.parse(xhr.response)
-        create_table(response.results)
+        ce.create_table(response.results)
+        // ce.create_graph(response.results)
         document.querySelector(".tab-buttons").click()
       }
     } catch(err) {
@@ -40,11 +43,11 @@
     var experiments = []
     var fieldsets   = Array.from(form.getElementsByTagName('fieldset'))
     
-    fieldsets_okay = fieldsets.every(fieldset => {
+    var fieldsets_okay = fieldsets.every(fieldset => {
       var experiment = {}
       var inputs     = Array.from(fieldset.getElementsByTagName('input')).concat(Array.from(fieldset.getElementsByTagName('select')))
       
-      inputs_okay = inputs.every(input => {
+      var inputs_okay = inputs.every(input => {
         if (input.value === '') { return false }
         
         experiment[[input.id]] = isNaN(input.value) ? input.value : Number(input.value)
@@ -66,74 +69,38 @@
   function add_exp() {
     var new_exp = document.createElement('fieldset')
 
-    new_exp.appendChild(create_label('Height:', 'height'))
-    new_exp.appendChild(create_input('number', 'height'))
+    new_exp.appendChild(ce.create_label('Height:', 'height'))
+    new_exp.appendChild(ce.create_input('number', 'height'))
 
-    new_exp.appendChild(create_label('Width:', 'width'))
-    new_exp.appendChild(create_input('number', 'width'))
+    new_exp.appendChild(ce.create_label('Width:', 'width'))
+    new_exp.appendChild(ce.create_input('number', 'width'))
 
-    new_exp.appendChild(create_label('Games:', 'nofgames'))
-    new_exp.appendChild(create_input('number', 'nofgames'))
+    new_exp.appendChild(ce.create_label('Games:', 'nofgames'))
+    new_exp.appendChild(ce.create_input('number', 'nofgames'))
 
-    new_exp.appendChild(create_label('Player 1:', 'player1'))
-    new_exp.appendChild(create_select('player1', [
+    new_exp.appendChild(ce.create_label('Player 1:', 'player1'))
+    var select = ce.create_select('player1', [
       { 'innerHTML' : 'Random',    'value' : 'PlayerRandom' },
       { 'innerHTML' : 'Minimax',   'value' : 'PlayerMinimax' },
       { 'innerHTML' : 'Rlearning', 'value' : 'PlayerRL' }
-    ]))
+    ])
+    select.onchange = is_minimax
+    new_exp.appendChild(select)
 
-    new_exp.appendChild(create_label('Player 2:', 'player2'))
-    new_exp.appendChild(create_select('player2', [
+    new_exp.appendChild(ce.create_label('Player 2:', 'player2'))
+    select = ce.create_select('player2', [
       { 'innerHTML' : 'Random',    'value' : 'PlayerRandom' },
       { 'innerHTML' : 'Minimax',   'value' : 'PlayerMinimax' },
       { 'innerHTML' : 'Rlearning', 'value' : 'PlayerRL' }
-    ]))
+    ])
+    select.onchange = is_minimax
+    new_exp.appendChild(select)
 
-    new_exp.appendChild(create_button('Delete', 'button'))
+    var button = ce.create_button('Delete', 'button')
+    button.onclick = del_exp
+    new_exp.appendChild(button)
 
     form.appendChild(new_exp)
-  }
-
-  function create_label(innerHTML, htmlFor) {
-    var label       = document.createElement('label')
-    label.htmlFor   = htmlFor
-    label.innerHTML = innerHTML
-    return label
-  }
-
-  function create_input(type, id) {
-    var input      = document.createElement('input')
-    input.type     = type
-    input.id       = id
-    input.name     = id
-    input.required = true
-    return input
-  }
-
-  function create_select(id, options) {
-    var select  = document.createElement('select')
-    select.id   = id
-    select.name = id
-    options.forEach(option => {
-      select.appendChild(create_option(option.innerHTML, option.value))
-    })
-    select.onchange = is_minimax
-    return select
-  }
-
-  function create_option(innerHTML, value) {
-    var option       = document.createElement('option')
-    option.innerHTML = innerHTML
-    option.value     = value
-    return option
-  }
-
-  function create_button(innerHTML, type) {
-    var button       = document.createElement('button')
-    button.innerHTML = innerHTML
-    button.type      = type
-    button.onclick   = del_exp
-    return button
   }
 
   function del_exp(e) {
@@ -146,14 +113,14 @@
     var player = (index == 7) ? 'p1' : 'p2'
 
     if (node.value === 'PlayerMinimax') {
-      node.parentNode.insertBefore(create_select(`${player}-heuristic`, [
+      node.parentNode.insertBefore(ce.create_select(`${player}-heuristic`, [
         { 'innerHTML' : 'Heuristic1',    'value' : 'heuristic1' },
         { 'innerHTML' : 'Heuristic2',    'value' : 'heuristic2' },
         { 'innerHTML' : 'Heuristic3',    'value' : 'heuristic3' },
       ]), node.parentNode.children[index+1])
-      node.parentNode.insertBefore(create_label(`${player.toUpperCase()} Heuristic:`, `${player}-heuristic`), node.parentNode.children[index+1])
-      node.parentNode.insertBefore(create_input('number', `${player}-depth`), node.parentNode.children[index+1])
-      node.parentNode.insertBefore(create_label(`${player.toUpperCase()} Depth:`, `${player}-depth`), node.parentNode.children[index+1])
+      node.parentNode.insertBefore(ce.create_label(`${player.toUpperCase()} Heuristic:`, `${player}-heuristic`), node.parentNode.children[index+1])
+      node.parentNode.insertBefore(ce.create_input('number', `${player}-depth`), node.parentNode.children[index+1])
+      node.parentNode.insertBefore(ce.create_label(`${player.toUpperCase()} Depth:`, `${player}-depth`), node.parentNode.children[index+1])
       node.mini_options_added = true
 
     } else if (node.mini_options_added) {
@@ -163,52 +130,6 @@
       node.parentNode.children[index+1].remove()
       node.mini_options_added = false
     }
-  }
-
-  function create_table(results) {
-    var section = document.querySelector('#results-table')
-
-    var previous_table = section.querySelector('table')
-    if (previous_table) { previous_table.remove() }
-    
-    var table = document.createElement('table')
-    table.appendChild(create_table_headers(results))
-
-    for (var i = 0; i < results.length; i++) {
-      row = create_row(i, results[i])
-      table.appendChild(row)
-    }
-
-    section.appendChild(table)
-  }
-
-  function create_table_headers(results) {
-    var row = document.createElement('tr')
-    row.appendChild(create_cell('th', '#'))
-
-    for (var key in results[0]) {
-      row.appendChild(create_cell('th', key.toUpperCase()))
-    }
-
-    return row
-  }
-  
-  function create_row(exp_num, results) {
-    var row = document.createElement('tr')
-    
-    row.appendChild(create_cell('td', exp_num))
-    
-    for (var key in results) {
-      row.appendChild(create_cell('td', results[key]))
-    }
-
-    return row
-  }
-
-  function create_cell(type, innerHTML) {
-    var cell       = document.createElement(type)
-    cell.innerHTML = innerHTML
-    return cell
   }
 
   function open_tab(e) {
---
+++ src/lib/create_element.js
@@ -0,0 +1,85 @@
+export function create_label(innerHTML, htmlFor) {
+  var label       = document.createElement('label')
+  label.htmlFor   = htmlFor
+  label.innerHTML = innerHTML
+  return label
+}
+
+export function create_input(type, id) {
+  var input      = document.createElement('input')
+  input.type     = type
+  input.id       = id
+  input.name     = id
+  input.required = true
+  return input
+}
+
+export function create_select(id, options) {
+  var select  = document.createElement('select')
+  select.id   = id
+  select.name = id
+  options.forEach(option => {
+    select.appendChild(create_option(option.innerHTML, option.value))
+  })
+  return select
+}
+
+export function create_option(innerHTML, value) {
+  var option       = document.createElement('option')
+  option.innerHTML = innerHTML
+  option.value     = value
+  return option
+}
+
+export function create_button(innerHTML, type) {
+  var button       = document.createElement('button')
+  button.innerHTML = innerHTML
+  button.type      = type
+  return button
+}
+
+
+export function create_table(results) {
+  var section = document.querySelector('#results-table')
+  
+  var previous_table = section.querySelector('table')
+  if (previous_table) { previous_table.remove() }
+  
+  var table = document.createElement('table')
+  table.appendChild(create_table_headers(results))
+  
+  for (var i = 0; i < results.length; i++) {
+    table.appendChild(create_row(i, results[i]))
+  }
+  
+  section.appendChild(table)
+}
+
+export function create_table_headers(results) {
+  var row = document.createElement('tr')
+  row.appendChild(create_cell('th', '#'))
+
+  for (var key in results[0]) {
+    row.appendChild(create_cell('th', key.toUpperCase()))
+  }
+
+  return row
+}
+
+export function create_row(exp_num, results) {
+  var row = document.createElement('tr')
+  
+  row.appendChild(create_cell('td', exp_num))
+  
+  for (var key in results) {
+    row.appendChild(create_cell('td', results[key]))
+  }
+
+  return row
+}
+
+export function create_cell(type, innerHTML) {
+  var cell       = document.createElement(type)
+  cell.innerHTML = innerHTML
+  return cell
+}
